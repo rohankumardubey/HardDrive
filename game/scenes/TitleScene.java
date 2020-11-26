6b349b0d7f6cac30370efd10bc9f774946d71fe9
Bryan McClain <bamcclain10@gmail.com>
2020-11-26 12:40:19 -0500

    Moved hints from loading scene to title scene

--- game/scenes/TitleScene.java
+++ game/scenes/TitleScene.java
@@ -9,6 +9,35 @@ import java.awt.*;
  */
 public class TitleScene extends Scene {
 
+  private int hintIndex = 0;
+
+  private static final Font HINT_FONT = new Font("monospace", Font.BOLD, 16);
+  private static final int HINT_DELAY = 75;
+  private static final String[] HINTS = new String[] {
+
+      "Use arrow keys to move, hold D to drift.",
+      "Drifting completely eliminates friction.",
+      "Drift around corners for a speed boost at the end!",
+      "The longer you drift, the bigger the speed boost!",
+      "Destroying a component heals you more if you hit it at higher speeds.",
+      "You only take damage from hitting a component if it doesn't break.",
+      "Drive through a large group of small components to quickly restore health.",
+      "Some obstacles are indestructible. Watch out!",
+      "Try drifting without accelerating to turn around without changing direction.",
+      "It takes a couple seconds to completely recover from a drift.",
+      "If you hit a boundary, you'll bounce off without taking damage.",
+      "The faster you're going when you hit a component, the more damage it takes.",
+      "Try drift boosting into components to deal extra damage!",
+      "Be careful; not every environment has the same friction!",
+      "Watch out for Anti-Viruses!",
+      "Drive faster to see more of the environment and find data files more easily.",
+      "Drift and accelerate while staying aimed at a specific point to do donuts!",
+      "Some computer chips can spawn enemies.",
+      "Cylindrical components roll more easily than they slide.",
+      "Try to avoid heavy components; some of them can't be destroyed in one hit!",
+      "You can toggle fullscreen mode using F4",
+      "Press Escape to exit the game at any time"};
+
   public TitleScene() {
     super(640, 480);
 
@@ -20,11 +49,15 @@ public class TitleScene extends Scene {
   protected void onCreate() {
     this.createEntity(new BinaryFlicker());
     this.createEntity(new ChangeSceneButton("Start", Color.GREEN, new LevelSelectScene(),
-                                            new Point2d(size.width / 2, size.height - 100)));
+                                            new Point2d(size.width / 2, size.height - 80)));
+
+    this.setTimer(1, HINT_DELAY, true);
   }
 
   @Override
-  protected void onTimer(int timerIndex) {}
+  protected void onTimer(int timerIndex) {
+    if (timerIndex == 1) { this.hintIndex = (hintIndex + 1) % HINTS.length; }
+  }
 
   @Override
   protected void onStep() {
@@ -48,5 +81,8 @@ public class TitleScene extends Scene {
     Dimension size = this.size;
     g2d.drawImage(GameAssets.getLoadedImage("data-file"), (size.width / 2) - 64,
                   (size.height / 2) - 64, 128, 128, null);
+
+    // Draw hint
+    Helpers.drawCenteredString(g2d, HINTS[hintIndex], new Rectangle(0, 310, 640, 50), HINT_FONT);
   }
 }
